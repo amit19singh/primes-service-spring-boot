026e0e7b5e6f59d39c4f2bf35d30065e83bae01a
Amit <amit19er@gmail.com>
2024-04-02 20:14:21 -0400

    For SE Class

--- src/test/java/com/amit/primesservices/PrimesServicesApplicationTests.java
+++ src/test/java/com/amit/primesservices/PrimesServicesApplicationTests.java
@@ -1,9 +1,11 @@
 package com.amit.primesservices;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class PrimesServicesApplicationTests {
 
 	@Test
